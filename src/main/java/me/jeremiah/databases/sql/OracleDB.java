e8d09eca6d8b82e19ffe464c1c4d3409acdb23e5
jerem <jeremiahhughes06@gmail.com>
2024-09-24 21:04:11 -0500

    OracleDB requires BigDecimal because it's a bitch...

--- src/main/java/me/jeremiah/databases/sql/OracleDB.java
+++ src/main/java/me/jeremiah/databases/sql/OracleDB.java
@@ -1,5 +1,13 @@
 package me.jeremiah.databases.sql;
 
+import me.jeremiah.Entry;
+import me.jeremiah.ExceptionManager;
+
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class OracleDB extends AbstractSQLDatabase {
 
   public OracleDB() {
@@ -13,6 +21,43 @@ public class OracleDB extends AbstractSQLDatabase {
   @Override
   public String getName() {
     return "OracleDB";
+  }
+
+  @Override
+  protected void parseInsert(Entry entry, PreparedStatement preparedStatement) throws SQLException {
+    preparedStatement.setInt(1, entry.getId());
+    preparedStatement.setString(2, entry.getFirstName());
+    preparedStatement.setString(3, String.valueOf(entry.getMiddleInitial()));
+    preparedStatement.setString(4, entry.getLastName());
+    preparedStatement.setInt(5, entry.getAge());
+    preparedStatement.setBigDecimal(6, BigDecimal.valueOf(entry.getNetWorth()));
+  }
+
+  @Override
+  protected void parseUpdate(Entry entry, PreparedStatement preparedStatement) throws SQLException {
+    preparedStatement.setString(1, entry.getFirstName());
+    preparedStatement.setString(2, String.valueOf(entry.getMiddleInitial()));
+    preparedStatement.setString(3, entry.getLastName());
+    preparedStatement.setInt(4, entry.getAge());
+    preparedStatement.setBigDecimal(5, BigDecimal.valueOf(entry.getNetWorth()));
+    preparedStatement.setInt(6, entry.getId());
+  }
+
+  @Override
+  protected Entry deserializeEntry(int id, ResultSet resultSet) {
+    try {
+      return new Entry(
+        id,
+        resultSet.getString("first_name"),
+        resultSet.getString("middle_initial").charAt(0),
+        resultSet.getString("last_name"),
+        resultSet.getInt("age"),
+        resultSet.getBigDecimal("net_worth").doubleValue()
+      );
+    } catch (SQLException exception) {
+      ExceptionManager.handleException(this, exception);
+    }
+    return null;
   }
 
 }
