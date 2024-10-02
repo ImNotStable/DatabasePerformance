7e688f62935cb2f4f7c362db86f1d466e21e50c4
jerem <jeremiahhughes06@gmail.com>
2024-10-02 13:00:00 -0500

    Oups...

--- src/main/java/me/jeremiah/databases/sql/MySQL.java
+++ src/main/java/me/jeremiah/databases/sql/MySQL.java
@@ -3,7 +3,7 @@ package me.jeremiah.databases.sql;
 public class MySQL extends AbstractSQLDatabase {
 
   public MySQL() {
-    super(org.mariadb.jdbc.Driver.class, "jdbc:mysql://localhost:3306/data");
+    super(com.mysql.cj.jdbc.Driver.class, "jdbc:mysql://localhost:3306/data");
     getConfig().setUsername("root");
   }
 
